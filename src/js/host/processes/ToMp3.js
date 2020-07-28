a05fcb6e84caa915a530c6fa691d7a28fa582c82
Yernemm <yrnemmizer@gmail.com>
2020-07-28 03:23:15 +0100

    sample tomp3

---
+++ src/js/host/processes/ToMp3.js
@@ -0,0 +1,16 @@
+const ffmpeg = require('ffmpeg');
+
+class ToMp3 {
+  constructor(){
+
+  }
+
+  run(input, config){
+      return new Promise((resolve, reject)=>{
+
+      });
+  }
+
+}
+
+module.exports = ToMp3;
